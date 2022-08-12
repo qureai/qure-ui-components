e823b095dfbd2c2e49f9e51b221f0bf28904eb38
abhijeet <abhijeet.sutar@qure.ai>
2022-08-12 16:01:16 +0530

    added more datepicker stories

--- src/composite/DatePicker/DatePicker.js
+++ src/composite/DatePicker/DatePicker.js
@@ -1,27 +1,36 @@
-import React, {useState} from 'react';
-import {DateRangePicker, DatePicker} from '@mantine/dates';
+import React, { useState } from "react";
+import {
+  DateRangePicker,
+  DatePicker as ManTineDatePicker,
+} from "@mantine/dates";
 import classNames from "classnames";
 
 function DatePicker(props) {
   const {
-    onDateChange, label, placeholder, datePickerContainerClass, className,
+    onDateChange,
+    value,
+    label,
+    placeholder,
+    datePickerContainerClass,
     isDisabled,
     errorMessage,
-    minDate, maxDate, dateFormat, isRange
-  } = props
-  const [value, setValue] = useState([
-    new Date(2021, 11, 1),
-    new Date(2021, 11, 5),
-  ]);
+    minDate,
+    maxDate,
+    dateFormat,
+    isRange,
+  } = props;
 
-  const onDateChange1 = (dates) => {
-    setValue(dates)
-    onDateChange(dates)
-  }
+  const _onDateChange = (dates) => {
+    onDateChange(dates);
+  };
 
-  if(isRange) {
+  if (isRange) {
     return (
-      <div className={classNames('date-wrapper', {[datePickerContainerClass]: datePickerContainerClass})}>
+      <div
+        className={classNames("date-wrapper", {
+          [datePickerContainerClass]: datePickerContainerClass,
+        })}
+      >
         <DateRangePicker
           label={label}
           placeholder={placeholder}
@@ -31,28 +40,28 @@ function DatePicker(props) {
           maxDate={maxDate}
           disabled={isDisabled}
           error={errorMessage}
-          onChange={onDateChange1}
+          onChange={_onDateChange}
         />
       </div>
-    )
+    );
   }
 
   return (
-      <DatePicker
-        classNames={{
-          wrapper: datePickerContainerClass
-        }}
-        label={label}
-        placeholder={placeholder}
-        value={value}
-        inputFormat={dateFormat}
-        minDate={minDate}
-        maxDate={maxDate}
-        disabled={isDisabled}
-        error={errorMessage}
-        onChange={onDateChange1}
-      />
-    )
+    <ManTineDatePicker
+      classNames={{
+        wrapper: datePickerContainerClass,
+      }}
+      label={label}
+      placeholder={placeholder}
+      value={value}
+      inputFormat={dateFormat}
+      minDate={minDate}
+      maxDate={maxDate}
+      disabled={isDisabled}
+      error={errorMessage}
+      onChange={_onDateChange}
+    />
+  );
 }
 
-export default DatePicker
+export default DatePicker;
